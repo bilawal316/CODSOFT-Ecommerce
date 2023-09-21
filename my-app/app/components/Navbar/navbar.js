31515dd873c4fcf36fe0b1a2106da318dc8894b0
bilawal316 <143614305+bilawal316@users.noreply.github.com>
2023-09-21 16:48:29 +0000

    modified:   app/components/Navbar/navbar.js

--- my-app/app/components/Navbar/navbar.js
+++ my-app/app/components/Navbar/navbar.js
@@ -4,10 +4,10 @@ import React from "react";
 const Navbar = () => {
   return (
     <div >
-        <ul className="flex justify-center cursor-pointer  space-x-4">
-            <li> Home </li>
-            <li> About </li>
-            <li> Contact Us </li>
+        <ul className="flex justify-center cursor-pointer  space-x-4 ">
+            <li className="hover:text-red-500"> Home </li>
+            <li className="hover:text-red-500"> About </li>
+            <li className="hover:text-red-500"> Contact Us </li>
             </ul>        
     </div>
   );
