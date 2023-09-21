3c3a0e63c5cd04250c4f58a7c35aab88db5c8960
bilawal316 <143614305+bilawal316@users.noreply.github.com>
2023-09-21 16:38:05 +0000

    Add untracked files

---
+++ my-app/app/components/Navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Navbar = () => {
+  return (
+    <div >
+        <ul className="flex justify-center cursor-pointer  space-x-4">
+            <li> Home </li>
+            <li> About </li>
+            <li> Contact Us </li>
+            </ul>        
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Page = () => {
+  return (
+   <div>
+    Heelow World
+   </div>
+  );
+};
+
+export default Page;
